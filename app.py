252b2717031e3fb348a41bd4fd8648085a305cb1
akritii-3107 <107861050+akritii-3107@users.noreply.github.com>
2023-03-24 13:46:26 +0530

    commit new2

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask,render_template,url_for
 
 app=Flask(__name__)
 
-base_url = "https://akritii-3107.github.io/LUMEN-website"
 
-@app.route(base_url+'/')
+
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route(base_url+'/about')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route(base_url+'/contact')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
