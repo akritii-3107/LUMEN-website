d437c3f8f9863944619071427381d5e44e296bb1
akritii-3107 <107861050+akritii-3107@users.noreply.github.com>
2023-03-24 13:54:30 +0530

    commit new

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ def about():
 def contact():
     return render_template('contact.html')
 
+from flask import send_from_directory
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
+
+
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
 
